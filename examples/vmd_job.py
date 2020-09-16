e2ca9bcf0ab3776f1a64f1efdfefe92b2aefe65d
mmancip <Martial.Mancip@MaisondelaSimulatipn.fr>
2020-09-16 11:24:08 +0200

    next tiles with actions.

--- examples/vmd_job.py
+++ examples/vmd_job.py
@@ -6,6 +6,12 @@ import sys,os,time
 import code
 import argparse
 import re, datetime
+import inspect
+    
+sys.path.append(os.path.realpath('/TiledViz/TVConnections/'))
+from connect import sock
+
+import json
     
 # HPC Machine working directory
 #In TVConnection :
@@ -27,6 +33,9 @@ def toggle_fullscr():
 
 
 if __name__ == '__main__':
+
+    actions_file=open("/home/myuser/actions.json",'r')
+    tiles_actions=json.load(actions_file)
 
     config = configparser.ConfigParser()
     config.optionxform = str
@@ -107,6 +116,7 @@ if __name__ == '__main__':
     print("Out of cp build_qr : %s" % (str(client.get_OK())))
 
     send_file_server(client,TileSet,"VMD","vmd_client",JOBPath)
+    send_file_server(client,TileSet,"VMD", "kill_vmd", JOBPath)
     send_file_server(client,TileSet,"VMD", "build_nodes_file", JOBPath)
     send_file_server(client,TileSet,"VMD", "script_vmd_Spezia.py", JOBPath)
     send_file_server(client,TileSet,"VMD", "script_vmd_Spezia.vmd", JOBPath)
@@ -185,7 +195,7 @@ if __name__ == '__main__':
 
     # TODO : give a list of lines !
     for i in range(NUM_DOCKERS):
-        launch_one_client(tileNum=i,file_name=file_name):
+        launch_one_client(tileNum=i)
     Last_Elt=NUM_DOCKERS-1
     
     def next_element(script='vmd_client',tileNum=-1,tileId='001'):
@@ -194,7 +204,7 @@ if __name__ == '__main__':
         COMMAND=' '+CASE_DOCKER_PATH+script+' '+DATA_PATH_DOCKER+' '+file_name
         COMMANDKill=' '+CASE_DOCKER_PATH+"kill_vmd"
         if ( tileNum > -1 ):
-            TilesStr=' Tiles=('+containerId(tileNum+1)+') '            
+            TilesStr=' Tiles=('+containerId(tileNum+1)+') '
         else:
             TilesStr=' Tiles=('+tileId+') '
         print("%d VMD command : %s" % (i,COMMAND))
@@ -231,7 +241,7 @@ if __name__ == '__main__':
     def fullscreenApp(windowname="VMD 1.9.2 OpenGL Display",tileNum=-1):
         movewindows(windowname=windowname,wmctrl_option='toggle,fullscreen',tileNum=tileNum)
 
-    def movewindows(windowname="VMD 1.9.2 OpenGL Display",wmctrl_option='toggle,fullscreen',tileNum=-1,tileId='001'):
+    def movewindows(windowname="VMD 1.9.2 OpenGL Display",wmctrl_option='toggle,fullscreen',tileNum=-1,tiéleId='001'):
         #remove,maximized_vert,maximized_horz
         #toggle,above
         #movewindows(windowname='glxgears',wmctrl_option="toggle,fullscreen",tileNum=2)
@@ -242,6 +252,24 @@ if __name__ == '__main__':
         client.send_server('execute TS='+TileSet+TilesStr+'/opt/movewindows '+windowname+' -b '+wmctrl_option)
         client.get_OK()
 
+    def kill_all_containers():
+        client.send_server('execute TS='+TileSet+' killall Xvnc')
+        print("Out of killall command : "+ str(client.get_OK()))
+        client.send_server('launch TS='+TileSet+" "+JOBPath+" "+COMMANDStop)
+        client.close()
+
+    # Launch Server for commands from FlaskDock
+    print("GetActions=ClientAction("+str(connectionId)+",globals=dict(globals()),locals=dict(**locals()))")
+    sys.stdout.flush()
+
+    try:
+        GetActions=ClientAction(connectionId,globals=dict(globals()),locals=dict(**locals()))
+        outHandler.flush()
+    except:
+        traceback.print_exc(file=sys.stdout)
+        code.interact(banner="Error ClientAction :",local=dict(globals(), **locals()))
+
+    print("Actions \n",str(tiles_actions))
 
     try:
         code.interact(banner="Code interact :",local=dict(globals(), **locals()))
@@ -259,26 +287,4 @@ if __name__ == '__main__':
     # 0x0140000d  0 935  747  499  147 vmd console
     # 0x01800009  0 602  -98  669  834 VMD 1.9.2 OpenGL Display
     # 0x01600004  0 6    26   470  190 VMD Main
-
-
-for i in range(NUM_DOCKERS):
-    client.send_server('execute TS='+TileSet+' Tiles=('+containerId(i+1)+') '+
-                       '/opt/movewindows OpenGL -b add,fullscreen')
-    client.get_OK()
-
-
-for i in range(NUM_DOCKERS):
-    client.send_server('execute TS='+TileSet+' Tiles=('+containerId(i+1)+') '+
-                       '/opt/movewindows  ??? -b remove,maximized_vert,maximized_horz')
-    client.get_OK()
-
-
-for i in range(NUM_DOCKERS):
-    client.send_server('execute TS='+TileSet+' Tiles=('+containerId(i+1)+') '+
-                       '/opt/movewindows ??? -b toggle,above')
-    client.get_OK()
-
-    
-client.send_server('execute TS='+TileSet+' killall Xvnc')
-print("Out of killall command : "+ str(client.get_OK()))
 
